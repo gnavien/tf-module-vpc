ba84c9ebeda71a20b8e79cfa5b3e9422ab9af05d
gnavien <gnavien@gmail.com>
2024-10-01 11:22:25 +0530

    update

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
+  cidr_block         = var.cidr_block
   enable_dns_support = true
+  tags = merge({
+    Name = "${var.env}-vpc"
+  },
+    var.tags)
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
