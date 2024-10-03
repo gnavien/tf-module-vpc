0dda791748e160bc918b0f425fffcd33188dd5e6
gnavien <gnavien@gmail.com>
2024-10-03 14:24:51 +0530

    update

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "cidr_block" {}
 variable "tags" {}
 variable "env"  {}
 variable "subnets" {}
-variable "vpc_id" {}
+#variable "vpc_id" {}
 
 variable "az" {
   default = ["us-east-1a", "us-east-1b"]
