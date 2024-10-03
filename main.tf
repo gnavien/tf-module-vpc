11325fcd035a700dda184af7d1e437b6fa5ec950
gnavien <gnavien@gmail.com>
2024-10-03 08:39:18 +0530

    update

--- main.tf
+++ main.tf
@@ -1,36 +1,41 @@
-resource "aws_vpc" "main" {
-  cidr_block         = var.cidr_block # cidr block will be sent to env main.tf
-  enable_dns_support = true
-  tags = merge({
-    Name = "${var.env}-vpc"
-  },
-    var.tags)  # this tag we are giving info in main.tfvars
-}
+#resource "aws_vpc" "main" {
+#  cidr_block         = var.cidr_block # cidr block will be sent to env main.tf
+#  enable_dns_support = true
+#  tags = merge({
+#    Name = "${var.env}-vpc"
+#  },
+#    var.tags)  # this tag we are giving info in main.tfvars
+#}
 
 # Local module for subnets
 
-module "subnets" {
-  source = "./subnets"
+#module "subnets" {
+#  source = "./subnets"
+#
+#  for_each    = var.subnets
+#  cidr_block  = each.value["cidr_block"]
+#  subnet_name = each.key
+#
+#  vpc_id = aws_vpc.main.id
+#
+#  env  = var.env
+#  tags = var.tags
+#  az   = var.az
+#}
+#
+#
+## 1st task is to create a AWS VPC peer connection
+#
+#
+#
+#resource "aws_vpc_peering_connection" "peer" {
+#  peer_vpc_id   = aws_vpc.main.id
+#  vpc_id        = var.default_vpc_id
+#  auto_accept   = true  # If we dont give auto accept as true peering connection will be in pending state
+#
+#}
 
-  for_each    = var.subnets
-  cidr_block  = each.value["cidr_block"]
-  subnet_name = each.key
-
-  vpc_id = aws_vpc.main.id
-
-  env  = var.env
-  tags = var.tags
-  az   = var.az
-}
-
-
-# 1st task is to create a AWS VPC peer connection
-
-
-
-resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = aws_vpc.main.id
-  vpc_id        = var.default_vpc_id
-  auto_accept   = true  # If we dont give auto accept as true peering connection will be in pending state
-
+resource "aws_vpc" "main" {
+  cidr_block         = var.cidr_block # cidr block will be sent to env main.tf
+  enable_dns_support = true
 }
